c0ea3d614befaa0ba7c20a0c9b0e67c91afcabd2
okmarcel <marcelgladysz44@gmail.com>
2025-11-12 13:22:11 +0100

    add basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <stdbool.h>
 #include <SDL3/SDL.h>
+#include "tree.h"
 
 #define WINDOW_TITLE "trees"
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 800
 
-int init(SDL_Window *window, SDL_Renderer *renderer);
+int init(SDL_Window **window, SDL_Renderer **renderer);
 
 int main(void) {
 	SDL_Window *window = nullptr;
 	SDL_Renderer *renderer = nullptr;
-	if (init(window, renderer))
+	if (init(&window, &renderer))
 		exit(EXIT_FAILURE);
+
+	Tree tree;
 
 	bool is_running = true;
 	SDL_Event e;
@@ -24,28 +27,37 @@ int main(void) {
 				break;
 			}
 		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		tree.grow();
+		tree.draw(renderer);
+		
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(50);
 	}
 
 	return EXIT_SUCCESS;
 }
 
-int init(SDL_Window *window, SDL_Renderer *renderer) {
+int init(SDL_Window **window, SDL_Renderer **renderer) {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		printf("%s\n", SDL_GetError());
 		return 1;
 	}
 
-	window = SDL_CreateWindow(WINDOW_TITLE,
+	*window = SDL_CreateWindow(WINDOW_TITLE,
 			WINDOW_WIDTH,
 			WINDOW_HEIGHT,
 			SDL_WINDOW_RESIZABLE);
-	if (window == nullptr) {
+	if (*window == nullptr) {
 		printf("%s\n", SDL_GetError());
 		return 1;
 	}
 
-    renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == nullptr) {
+    *renderer = SDL_CreateRenderer(*window, NULL);
+    if (*renderer == nullptr) {
 		printf("%s", SDL_GetError());
 		return 1;
     }
---
+++ src/tree.cpp
@@ -0,0 +1,68 @@
+#include "tree.h"
+#include <SDL3/SDL.h>
+#include <cmath>
+#include <cstdlib>
+
+#define DEFAULT_MAX_DEPTH 3  // VALUE FOR TESTING
+#define DEFAULT_MAX_LENGTH 301
+#define GROWTH_INCREMENT 5
+#define WHITE_COLOR 255, 255, 255, 255
+#define GREEN_COLOR 0, 180, 0, 255;
+
+Branch::Branch() {
+	max_length = DEFAULT_MAX_LENGTH;
+	length = 0.0f;
+	angle = M_PI * 2.0f * (static_cast<float>(rand()) / RAND_MAX - 0.5f);
+}
+void Branch::draw(SDL_Renderer *renderer, int x, int y, float parent_angle) {
+	float total_angle = angle + parent_angle;
+	int x2 = x + length * std::sin(total_angle);
+	int y2 = y + length * std::cos(total_angle);
+
+	SDL_SetRenderDrawColor(renderer, WHITE_COLOR);
+
+	SDL_RenderLine(renderer, x, y, x2, y2);
+
+	for (auto& child : children) {
+		child->draw(renderer, x2, y2, total_angle);
+	}
+	
+	return;
+}
+void Branch::grow(int depth) {
+	if (length < max_length) {
+		length += GROWTH_INCREMENT;
+	} else {
+		if (depth <= 1) return;
+		if (children_count == 0) {
+			children_count = 2 + rand() % 2;
+			for (int i = 0; i < children_count; i++) {
+				auto child = std::make_unique<Branch>();
+				children.push_back(std::move(child));
+			}
+		}
+		for (auto& child : children) {
+			child->grow(depth - 1);
+		}
+	}
+	return;
+}
+
+Tree::Tree() {
+	max_depth = DEFAULT_MAX_DEPTH;
+	root = std::make_unique<Branch>();
+}
+void Tree::draw(SDL_Renderer *renderer) {
+	if (!root) return;
+	
+	int rwidth, rheight;
+	SDL_GetCurrentRenderOutputSize(renderer, &rwidth, &rheight);
+
+	root->draw(renderer, rwidth / 2.0f, rheight, 0.0f); 
+}
+void Tree::grow() {
+	if (!root) return;
+	root->grow(max_depth);
+	return;
+}
+
